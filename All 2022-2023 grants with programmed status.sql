044b1df15645ebb5a76777e9bd305fd0c8b5f507
munsifhusami1 <munsif.husami@gmail.com>
2026-07-02 16:51:11 -0400

    Update All 2022-2023 grants with programmed status.sql

--- All 2022-2023 grants with programmed status.sql
+++ All 2022-2023 grants with programmed status.sql
@@ -1,22 +1,17 @@
-Use DB1
 SELECT 
      [Awarded_FY]
       ,[Agency]
       ,[Grant_Classification]
       ,[Program]
       ,[Project_Title]
-      ,[Description_Summary]
-      ,[TIP_DBNUM]
       ,[Grant_Type]
       ,[Amount]
       ,CASE
-      WHEN Programmed = 1 THEN 'Yes'
-      WHEN Programmed = 0 THEN 'No'
-      END AS [Programmed],
-      [County_ies]
-      ,[Municipality]
-      ,[Recipient]
+      WHEN [Obligated?_(Y/N)] = 'Y' THEN 'Yes'
+      ELSE 'No'
+      END AS [Obligated]
+      ,[County_ies]
 FROM dbo.[Grants Tracker]
 WHERE [Awarded_FY] IN ('2022', '2023')
-AND [Obligated] != 1
+AND [Obligated?_(Y/N)] = 'N'
 ORDER BY [Awarded_FY] ASC
