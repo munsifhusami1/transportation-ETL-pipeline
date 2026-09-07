97075cea17c6a9257de6cb352cf51f6a7bbaf020
munsifhusami1 <munsif.husami@gmail.com>
2026-09-07 15:48:12 -0400

    Update extract.py

--- functions/extract.py
+++ functions/extract.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-"""Extract fetches grant application data from source CSV"""
 
 def extract(filepath):
+    """Extract fetches grant application data from source CSV"""
     df = pd.read_csv(filepath, encoding='latin-1')
     return df
