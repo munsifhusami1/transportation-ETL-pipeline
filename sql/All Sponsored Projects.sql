b2f605749cc1e34beff5142ca0c1e1fb00e364a4
munsifhusami1 <munsif.husami@gmail.com>
2026-09-07 15:53:01 -0400

    Create All Sponsored Projects.sql

---
+++ sql/All Sponsored Projects.sql
@@ -0,0 +1,13 @@
+SELECT
+     [Awarded_FY]
+      ,[Agency]
+      ,[Grant_Classification]
+      ,[Program]
+      ,[Project_Title]
+      ,[Grant_Type]
+      ,[Amount]
+      ,[Obligated_to_Date]
+      ,[Obligated?_(Y/N)]
+      ,[County_ies]
+FROM dbo.[Grants Tracker]
+ORDER BY [Awarded_FY] ASC
